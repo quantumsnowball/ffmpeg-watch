00530e10f71d02b746e392cf49d4876f519367db
Quantum Snowball <quantum.snowball@gmail.com>
2024-03-27 01:16:41 +0800

    should and should not run

--- ffmpeg_watch/cli.py
+++ ffmpeg_watch/cli.py
@@ -3,18 +3,6 @@ import sys
 from ffmpeg_watch.default import run_default_ffmpeg
 from ffmpeg_watch.watch import run_ffmpeg_watch
 
-'''
-ss to  t case
- 0  0  1 bar dur=t
- 0  1  1 def dur=t(override to)
- 1  0  1 bar dur=t
- 1  1  1 def dur=t(override to)
- 0  1  0 bar dur=to
- 1  1  0 bar dur=to-ss
- 0  0  0 bar dur=full
- 1  0  0 bar dur=full-ss
-'''
-
 
 def main() -> None:
     # verify args
@@ -30,7 +18,7 @@ def main() -> None:
     t = args.count('-t')
 
     # any invalid time flag count will go to default
-    if any(0 <= count <= 1 for count in (ss, to, t)):
+    if any(count > 1 for count in (ss, to, t)):
         return run_default_ffmpeg(args)
 
     # if t flag, duration is know
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,17 +12,50 @@ def test_main(script_runner: ScriptRunner):
 
 
 def test_multiple_i_flag(script_runner: ScriptRunner):
-    '''
-    ss to  t target
-     0  0  1 bar dur=t
-     0  1  1 def dur=t(override to)
-     1  0  1 bar dur=t
-     1  1  1 def dur=t(override to)
-     0  1  0 bar dur=to
-     1  1  0 bar dur=to-ss
-     0  0  0 bar dur=full
-     1  0  0 bar dur=full-ss
-    '''
     with patch('ffmpeg_watch.cli.run_default_ffmpeg') as mock_fn:
         script_runner.run(['ffmpeg-watch', '-i', 'input1.mp4', '-i', 'input2.mp4', 'output.mp4'])
         mock_fn.assert_called_once_with(['-i', 'input1.mp4', '-i', 'input2.mp4', 'output.mp4'])
+
+
+@pytest.mark.parametrize('ss, to, t, supported', [
+    (0, 0, 1, True),
+    (0, 1, 1, True),
+    (1, 0, 1, True),
+    (1, 1, 1, True),
+    (0, 1, 0, True),
+    (1, 1, 0, True),
+    (0, 0, 0, True),
+    (1, 0, 1, True),
+
+    (0, 2, 1, False),
+])
+def test_time_opts(script_runner: ScriptRunner, ss, to, t, supported):
+    '''
+    ss to  t  solution
+     0  0  1  dur=t
+     0  1  1  dur=t(override to)
+     1  0  1  dur=t
+     1  1  1  dur=t(override to)
+     0  1  0  dur=to
+     1  1  0  dur=to-ss
+     0  0  0  dur=full
+     1  0  0  dur=full-ss
+    '''
+    command = ['ffmpeg-watch']
+    args = ['-i', 'input.mp4']
+    for _ in range(ss):
+        args += ['-ss', '01:23:45']
+    for _ in range(to):
+        args += ['-to', '01:23:45']
+    for _ in range(t):
+        args += ['-t', '01:23:45']
+    args += ['output.mp4']
+
+    fns = ('run_ffmpeg_watch', 'run_default_ffmpeg')
+    should_run, should_not_run = fns if supported else reversed(fns)
+
+    with (patch(f'ffmpeg_watch.cli.{should_run}') as should_run_mock,
+          patch(f'ffmpeg_watch.cli.{should_not_run}') as should_not_run_mock):
+        script_runner.run(command + args)
+        should_run_mock.assert_called_once_with(args)
+        should_not_run_mock.assert_not_called()
