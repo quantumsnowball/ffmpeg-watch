3919b4d40af5518bb5f6f6097e3a448fc4a31183
Quantum Snowball <quantum.snowball@gmail.com>
2024-03-26 19:43:53 +0800

    todo

--- ffmpeg_watch/cli.py
+++ ffmpeg_watch/cli.py
@@ -3,8 +3,21 @@ from subprocess import Popen
 
 
 def main() -> None:
+    # verify args
     args = sys.argv[1:]
-    cmd = ['ffmpeg'] + args
+
+    # create ffmpeg command
+    cmd = ['ffmpeg']
+    cmd += args
+    cmd[1:1] = ['-progress', 'pipe:1']
+    cmd[1:1] = ['-stats_period', '0.1']
+
+    # run ffmpeg in process
     proc = Popen(cmd)
     proc.wait()
+
+    # decide if capable to display progress bar or not
+    # read through PIPE and display progress bar
+
+    # retain return code
     sys.exit(proc.returncode)
